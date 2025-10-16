f4cb4e540dcedb8ca8373e3ae316431909fec584
khainm <Minhkkhai322@gmail.com>
2025-10-16 11:09:38 +0700

    feat: Add vite-plugin-sitemap for automated sitemap generation and configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap';
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://greenweave.vn',
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/products',
+        '/contact',
+        '/cart',
+        '/checkout',
+      ],
+      changefreq: 'daily',
+      priority: 0.8,
+    }),
   ],
   server: {
     proxy: {
